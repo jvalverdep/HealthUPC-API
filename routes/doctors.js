6857ce59c15900ee10867e7f9ebf4f6fccbf15ca
Javier Valverde Pairazamán <javier.valverde.pa@gmail.com>
2018-06-30 02:54:41 -0500

    appointments for doctors

--- routes/doctors.js
+++ routes/doctors.js
@@ -5,6 +5,20 @@ function isInt(value) {
            parseInt(Number(value)) == value && 
            !isNaN(parseInt(value, 10));
 }
+function parser(json) {
+    let newObject = {};
+    Object.keys(json).forEach(key => {
+      const newKeys = key.split(".");
+      const lastKey = newKeys.pop();
+      let pointer = newObject;
+      newKeys.forEach(newKey => {
+        if (typeof pointer[newKey] !== "object") pointer[newKey] = {};
+        pointer = pointer[newKey];
+      });
+      pointer[lastKey] = json[key];
+    });
+    return newObject;
+}
 
 module.exports = function(app) {
     app.route('/doctors/work_hours')
@@ -50,9 +64,13 @@ module.exports = function(app) {
             }
             sequelize.query('CALL sp_getDoctorsWorkHoursByDay(?)', { replacements: [query.d], type: sequelize.QueryTypes.SELECT })
             .spread((results, _) => {
-                return Object.keys(results).length !== 0 ? res.status(200).json(lodash.values(results)) : res.status(404).json({ 'error': 'Couldn\'t fetch any doctor' });
+                if (Object.keys(results).length !== 0) {
+                    return res.status(200).json(lodash.values(results))
+                } else {
+                    return res.status(404).json({ 'error': 'Couldn\'t fetch any doctor' });
+                }
             }).catch((error) => {
-                return res.status(500).json({ 'error': error.message });
+                return res.json({ 'error': error.message });
             });
         }, async (req, res, next) => {
             const db = req.app.db;
@@ -76,26 +94,26 @@ module.exports = function(app) {
     app.route('/doctors/:id')
         .get(async (req, res, next) => {
             const db = req.app.db;
-            const Op = db.Sequelize.Op;
+            const sequelize = db.sequelize;
             const doctorId = req.params.id;
             if (!isInt(doctorId)) {
                 next();
                 return;
             }
-            db.user.findOne({ 
-                attributes: [ 'id', 'first_name', 'last_name', 'email', 'profession' ],
-                include: { model: db.operation_time, as: 'operation_times' },
-                where: { id: { [Op.eq]: doctorId }, rol_id: { [Op.eq]: '1' }}
-            })
-            .then(doctor => {
-                if (!doctor) {
-                    return res.status(404).json({ 'error': 'Doctor not found' })
-                }
-                return res.status(200).json(doctor);
-            })
-            .catch(error => {
-                return next(error);
-            })
+
+            sequelize.query('CALL sp_getDoctorsAppointments(?)', { replacements: [doctorId], type: sequelize.QueryTypes.SELECT })
+                .spread((results, _) => {
+                    if (Object.keys(results).length !== 0) {
+                        let r = lodash.values(results);
+                        const output = r.map(i => parser(i));
+                        return res.status(200).json(output);
+                    } else {
+                        return res.status(404).json({ 'error': 'Couldn\'t fetch any doctor' });
+                    }
+                }).catch((error) => {
+                    return res.json({ 'error': error.message });
+                });
+
         })
     
 }
